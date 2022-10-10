a24b8ac146bb732e2ce3137edcf8b1a005599544
Sagar Pol <76035885+sagarpol3008@users.noreply.github.com>
2022-10-10 15:39:10 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict";
 let cartIcon = document.querySelector("#cart-icon");
 let cart = document.querySelector(".cart");
 let closeCart = document.querySelector("#close-cart");
@@ -28,6 +29,18 @@ function ready() {
     var buttton = removeCartButtons[i];
     buttton.addEventListener("click", removeCartItem);
   }
+  //QUANTITY CHANGES
+  var quantityInputs = document.getElementsByClassName("cart-quantity");
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+  //ADD TO CART
+  var addCart = document.getElementsByClassName("add-cart");
+  for (var i = 0; i < addCart.length; i++) {
+    var button = addCart[i];
+    button.addEventListener("click", addCartClicked);
+  }
 }
 
 //REMOVE ITEMS FROM CART
@@ -35,6 +48,23 @@ function removeCartItem(event) {
   var buttonClicked = event.target;
   buttonClicked.parentElement.remove();
   updateTotal();
+}
+
+//QUANTITY CHANGES
+
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateTotal();
+}
+//ADD TO CART
+function addCartClicked(event) {
+  var button = event.target;
+  var shopProducts = button.parentElement;
+  var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+  console.log(title);
 }
 
 //UPDATE TOTAL
@@ -49,6 +79,8 @@ function updateTotal() {
     var price = parseFloat(priceElement.innerText.replace("$", ""));
     var quantity = quantityElement.value;
     total = total + price * quantity;
+    //IF PRICE CONTAIN SOME CENTS VALUE
+    total = Math.round(total * 100) / 100;
 
     document.getElementsByClassName("total-price")[0].innerText = "$" + total;
   }
